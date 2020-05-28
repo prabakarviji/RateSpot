7a6f935c544cf51f9c4dc6c890aabb08f9031ea8
prabakarviji <prabakarviji@gmail.com>
2020-05-28 23:29:59 +0530

    Rating: Activity added

---
+++ app/src/main/java/app/prabs/ratespot/RatingActivity.kt
@@ -0,0 +1,11 @@
+package app.prabs.ratespot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating)
+    }
+}
