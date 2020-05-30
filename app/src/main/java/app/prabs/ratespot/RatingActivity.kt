5695047115f3ad270382495b28a3dcf8e6d9cc92
prabakarviji <prabakarviji@gmail.com>
2020-05-30 13:46:30 +0530

    Animation: Added to rating bar

--- app/src/main/java/app/prabs/ratespot/RatingActivity.kt
+++ app/src/main/java/app/prabs/ratespot/RatingActivity.kt
@@ -1,11 +1,33 @@
 package app.prabs.ratespot
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Log
+import android.widget.RatingBar
+import android.widget.RatingBar.OnRatingBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import app.prabs.ratespot.databinding.ActivityRatingBinding
 
-class RatingActivity : AppCompatActivity() {
+
+class RatingActivity : AppCompatActivity(), OnRatingBarChangeListener {
+
+    private lateinit var binding: ActivityRatingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rating)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_rating)
+        binding.roadRating.onRatingBarChangeListener = this
+        binding.lightRating.onRatingBarChangeListener = this
     }
+
+    override fun onRatingChanged(ratingBar: RatingBar?, rating: Float,fromUser: Boolean) {
+        if (fromUser){
+            val anim = ObjectAnimator.ofFloat(ratingBar, "rating",0f, rating)
+            anim.duration = 300
+            anim.start()
+        }
+    }
+
+
 }
