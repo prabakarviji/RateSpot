ac2808ee1ea642220bcdd5536135a846f3fa4858
prabakarviji <prabakarviji@gmail.com>
2020-06-03 01:24:41 +0530

    Custom view on click to color change

--- app/src/main/java/app/prabs/ratespot/RatingView.kt
+++ app/src/main/java/app/prabs/ratespot/RatingView.kt
@@ -6,7 +6,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Toast
+import androidx.core.content.withStyledAttributes
 
 
 private enum class Rating(val value: Int) {
@@ -17,14 +17,15 @@ private enum class Rating(val value: Int) {
     GOOD(R.string.good),
     EXCELLENT(R.string.excellent);
 
-    fun next() = when (this) {
-        EMPTY -> TERRIBLE
-        TERRIBLE -> BAD
-        BAD -> AVERAGE
-        AVERAGE -> GOOD
-        GOOD -> EXCELLENT
-        EXCELLENT -> EMPTY
+    fun find(num:Int) = when (num) {
+        0 -> TERRIBLE
+        1 -> BAD
+        2 -> AVERAGE
+        3 -> GOOD
+        4 -> EXCELLENT
+        else -> TERRIBLE
     }
+
 }
 
 class RatingView @JvmOverloads constructor(
@@ -33,9 +34,14 @@ class RatingView @JvmOverloads constructor(
 
     init {
         isClickable = true
+        context.withStyledAttributes(attrs, R.styleable.RateView) {
+            terribleColor = getColor(R.styleable.RateView_terribleColor, 0)
+            badColor = getColor(R.styleable.RateView_badColor, 0)
+            averageColor = getColor(R.styleable.RateView_averageColor, 0)
+            goodColor = getColor(R.styleable.RateView_goodColor, 0)
+            excellentColor = getColor(R.styleable.RateView_excellentColor, 0)
+        }
     }
-
-    private var rating = Rating.EMPTY
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
@@ -46,6 +52,7 @@ class RatingView @JvmOverloads constructor(
             }
             MotionEvent.ACTION_UP -> {
                 var position = event.x/(width/5)
+                rating = rating.find(position.toInt())
                 performClick()
                 return true
             }
@@ -55,17 +62,19 @@ class RatingView @JvmOverloads constructor(
 
     override fun performClick(): Boolean {
         if (super.performClick()) return true
-
-        rating = rating.next()
-        //contentDescription = resources.getString(fanSpeed.label)
-
         invalidate()
         return true
     }
 
     private var rectHeight = 80
-    private var fanSpeed = Rating.TERRIBLE
+    private var rating = Rating.EMPTY
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
+
+    private var terribleColor = 0
+    private var badColor = 0
+    private var averageColor = 0
+    private var goodColor = 0
+    private var excellentColor = 0
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
@@ -78,9 +87,24 @@ class RatingView @JvmOverloads constructor(
     }
 
     private fun PointF.computeXYForSpeed(pos: Rating) {
-        var distance = width/5
-        x = (pos.ordinal * distance).toFloat()
+        var unitWidth = width/5
+        x = (pos.ordinal * unitWidth).toFloat()
         y = height.toFloat()
+    }
+
+    private fun findColor(i: Rating): Int {
+        var color = Color.TRANSPARENT
+        if(rating > i) {
+            color = when (rating) {
+                Rating.TERRIBLE -> terribleColor
+                Rating.BAD -> badColor
+                Rating.AVERAGE -> averageColor
+                Rating.GOOD -> goodColor
+                Rating.EXCELLENT -> excellentColor
+                else -> Color.TRANSPARENT
+            }
+        }
+        return color
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -88,21 +112,19 @@ class RatingView @JvmOverloads constructor(
         paint.color = Color.WHITE
         paint.style = Paint.Style.FILL
         canvas.drawRect(0f, 0f, width.toFloat (), height.toFloat(),paint)
-        paint.color = Color.GRAY
+        paint.color = Color.BLACK
         paint.style = Paint.Style.STROKE
         canvas.drawRect(0f, 0f, width.toFloat (), height.toFloat(),paint)
         for (i in Rating.values()) {
             pointPosition.computeXYForSpeed(i)
-            if(i.ordinal != 0 && i.ordinal != 5) {
-                paint.color = Color.GRAY
-                canvas.drawLine(pointPosition.x, 0f, pointPosition.x, pointPosition.y, paint)
-
-            }
-            else{
-                paint.color = Color.RED
-                paint.style = Paint.Style.FILL
-                canvas.drawRect(0f, 0f, width/5.toFloat (), height.toFloat(),paint)
-            }
+            paint.color = findColor(i)
+            paint.style = Paint.Style.FILL
+            canvas.drawRect(pointPosition.x, 0f, pointPosition.x+(width/5), pointPosition.y, paint)
+            paint.color = Color.BLACK
+            paint.style = Paint.Style.STROKE
+            canvas.drawRect(pointPosition.x, 0f, pointPosition.x+(width/5), pointPosition.y, paint)
+            paint.color = if (i == Rating.EMPTY || i == Rating.EXCELLENT) Color.TRANSPARENT else Color.BLACK
+            canvas.drawLine(pointPosition.x, 0f, pointPosition.x, pointPosition.y, paint)
         }
 
     }
