1e027da308e83e66f3b0f88fefee23aac033d7d8
prabakarviji <prabakarviji@gmail.com>
2020-06-03 23:01:49 +0530

    Handle callback results

--- app/src/main/java/app/prabs/ratespot/RatingActivity.kt
+++ app/src/main/java/app/prabs/ratespot/RatingActivity.kt
@@ -1,30 +1,27 @@
 package app.prabs.ratespot
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.util.Log
-import android.widget.RatingBar
-import android.widget.RatingBar.OnRatingBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import app.prabs.ratespot.databinding.ActivityRatingBinding
 
 
-class RatingActivity : AppCompatActivity(), OnRatingBarChangeListener {
+class RatingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRatingBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_rating)
-    }
+        binding.roadRating.setRatingViewListener(object : RatingView.RatingViewListener {
 
-    override fun onRatingChanged(ratingBar: RatingBar?, rating: Float,fromUser: Boolean) {
-        if (fromUser){
-            val anim = ObjectAnimator.ofFloat(ratingBar, "rating",0f, rating)
-            anim.duration = 300
-            anim.start()
-        }
+            override fun setRating(value: Int) {
+                binding.roadValue.text = value.toFloat().toString()
+            }
+        })
+
     }
 
 
